65bbb7574ef7335598c6bb297899dfce681eb37f
Ryan <agingllama@gmail.com>
2018-12-31 19:48:28 -0500

    First version of my Fighting Game.

--- fighters.py
+++ fighters.py
@@ -14,12 +14,13 @@ class Test(pg.sprite.Sprite):
     def __init__(self, game):
         self.game = game
         pg.sprite.Sprite.__init__(self)
-        # IMAGE IS NOT READY
-        # self.image = pg.image.load(path.join(FIGHTERS, 'TestFighter.png')).convert()
-        # self.image = pg.transform.scale(self.image, (60, 100))
-        # self.image.set_colorkey(BLACK)
-        self.image = pg.Surface((60, 100))
-        self.image.fill(RED)
+        # Temp images
+        img = pg.image.load(path.join(FIGHTERS, 'TestFighter.png')).convert()
+        self.STANDR = pg.transform.scale(img, (60, 100))
+        self.STANDL = pg.transform.flip(self.STANDR, True, False)
+        # TEST IMAGE
+        self.image = self.STANDR
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = CENTER
         self.posx = WIDTH / 2
@@ -28,6 +29,7 @@ class Test(pg.sprite.Sprite):
         self.ay = 0
         self.vx = 0
         self.vy = 0
+        self.direction = 'r'
 
     def jump(self):
         # If fighter is on the ground, jumps
@@ -61,7 +63,20 @@ class Test(pg.sprite.Sprite):
         self.rect.y -= direction
         return hits
 
+    def animate(self):
+        # Will animate in the future
+        # For now this controls the fighter direction
+        if self.vx != 0:
+            if self.vx > 0:
+                self.image = self.STANDR
+                self.direction = 'r'
+            else:
+                self.image = self.STANDL
+                self.direction = 'l'
+            self.image.set_colorkey(BLACK)
+
     def update(self):
+        self.animate()
         # Resets acceleration and applies gravity
         self.ax = 0
         self.ay = Test.GRAVITY
@@ -73,12 +88,13 @@ class Test(pg.sprite.Sprite):
             self.ax = Test.ACCEL
         # Applies friction
         self.ax += self.vx * Test.FRICTION
-        # Stops slow crawl
-        if abs(self.vx) < 0.1:
-            self.vx = 0
         # Applies acceleration
         self.vx += self.ax
         self.vy += self.ay
+        # Stops slow crawl
+        if abs(self.vx) < 0.2:
+            self.vx = 0
+            self.ax = 0
         # Applies velocity and sets position
         self.rect.x += self.vx + self.ax * 0.5
         self.rect.y += self.vy + self.ay * 0.5
--- weapons.py
+++ weapons.py
@@ -12,6 +12,21 @@ class TestWeapon(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.image = pg.transform.rotate(self.image, 90)
         self.rect.topleft = self.fighter.rect.center
+        self.direction = 'r'
+
+    def flip(self):
+        # Flips the sprite
+        if self.direction == 'r':
+            self.direction = 'l'
+        else:
+            self.direction = 'r'
+        self.image = pg.transform.flip(self.image, True, False)
 
     def update(self):
-        self.rect.topleft = self.fighter.rect.center
+        # Flips sprite if fighter changes directions
+        if self.direction != self.fighter.direction:
+            self.flip()
+        if self.direction == 'r':
+            self.rect.topleft = self.fighter.rect.center
+        else:
+            self.rect.topright = self.fighter.rect.midleft
