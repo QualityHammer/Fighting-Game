caca8455ffcc22b74139f84f4bebfe3aa181cc89
Ryan <agingllama@gmail.com>
2019-01-03 14:52:44 -0500

    Added temporary floor texture

--- objects.py
+++ objects.py
@@ -8,8 +8,7 @@ class Ground(pg.sprite.Sprite):
 
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((WIDTH, 100))
-        self.image.fill(GREEN)
+        self.image = pg.image.load(path.join(OBJECTS, 'Floor.png')).convert()
         self.rect = self.image.get_rect()
         self.rect.bottomleft = (0, HEIGHT)
 
