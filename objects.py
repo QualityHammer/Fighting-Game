c3f8e6f38f3216a9ee71f4b454aa7bf81ed5a09e
Ryan <agingllama@gmail.com>
2019-01-03 15:13:31 -0500

    Changed game resolution to 1500 x 900 and scaled up wall and floor textures

--- objects.py
+++ objects.py
@@ -9,6 +9,7 @@ class Ground(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load(path.join(OBJECTS, 'Floor.png')).convert()
+        self.image = pg.transform.scale(self.image, (1500, 166))
         self.rect = self.image.get_rect()
         self.rect.bottomleft = (0, HEIGHT)
 
@@ -21,12 +22,16 @@ class Wall(pg.sprite.Sprite):
     def __init__(self, direction):
         pg.sprite.Sprite.__init__(self)
         self.direction = direction
-        self.image = pg.Surface((100, HEIGHT - 100))
-        self.image.fill(BLUE)
+        self.image = pg.image.load(path.join(OBJECTS, 'Wall.png')).convert()
+        # TODO: FIX WALL TEXTURE SCALING
+        self.image = pg.transform.scale(self.image, (166, 734))
         self.rect = self.image.get_rect()
         if direction == 'left':
+            self.image = self.image
             self.rect.topleft = (0, 0)
         elif direction == 'right':
+            self.image = pg.transform.flip(self.image, True, False)
             self.rect.topright = (WIDTH, 0)
         else:
-            print('ERROR: {} has a false direction')
+            # If wall has no direction
+            print('ERROR: {} has a false direction').format(self)
